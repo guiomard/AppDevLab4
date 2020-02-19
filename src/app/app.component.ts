226101f034ff197a7b34d551c0ff5f36146621aa
GAVIN GUIOMARD - STUDENT <G00358077@gmit.ie>
2020-02-19 15:29:52 +0000

    lab 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count: number = 0;
+  myString: string = null;
+  star2 = true;
+  names: string[] = ["marvin", "garvin"];
+
+  buttonClick()  {
+    console.log("You clicked the button");
+    this.count++;
+  }
+
+  doubleClick(){    
+
+    if(this.myString == null){
+      this.myString = "Look at the star!";
+    }
+    else{
+      this.myString = null;
+    }
+
+    if(this.star2 == true){
+      this.star2 = false;
+    }
+    else{
+      this.star2 = true;
+    }
+  }
 }
